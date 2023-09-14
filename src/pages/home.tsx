fd41d3fbbb9360e0adf41dbe21c88ffdbb678c09
Marc Brophy <aldrichbrophy@gmail.com>
2023-09-14 12:43:24 -0700

    parsed file and changed csv

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,17 @@
-
-import test_20 from '/test_20.csv';
+import Papa from 'papaparse';
 
 function HomePage() {
-    const reader = new FileReader();
-    reader.onload = () => {
-        const text = reader.result;
-        if(text) {
-            console.log("Found text");
-        }
-    }
 
-    reader.readAsText(test_20);
+    Papa.parse('/data.csv', {
+        download: true,
+        step: function(row) {
+            console.log("Row:", row.data);
+        },
+        complete: function() {
+            console.log("Complete.")
+        }
+    })
+    
     return (
         <>
         <h1>test</h1>
