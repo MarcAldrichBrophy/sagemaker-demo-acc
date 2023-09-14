fa32d0b9b23b4ef30e4cd5fe717153baf6257ecd
Marc Brophy <aldrichbrophy@gmail.com>
2023-09-14 11:46:17 -0700

    updated packages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,16 @@
 
+import test_20 from '/test_20.csv';
 
 function HomePage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+        const text = reader.result;
+        if(text) {
+            console.log("Found text");
+        }
+    }
+
+    reader.readAsText(test_20);
     return (
         <>
         <h1>test</h1>
